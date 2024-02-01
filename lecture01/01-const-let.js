7c4c1a07035529c8662ff8f1b2c57c3847e45d86
Grum5 <126805013+Grum5@users.noreply.github.com>
2024-01-31 19:30:33 -0800

    added Lecture01

---
+++ lecture01/01-const-let.js
@@ -0,0 +1,14 @@
+
+let maxNumber = 40
+let randomNumber = Math.floor(Math.random() * maxNumber)
+
+console.log(randomNumber)
+
+if (randomNumber == 21) {
+	console.log("Tu numero es " + randomNumber + "\n Ganaste")
+} else if (randomNumber > 21){
+    console.log("Tu numero es " + randomNumber + "\nPor lo tanto es mayor a 21\nPerdiste")
+} else {
+    console.log("Tu numero es " + randomNumber)
+}
+
