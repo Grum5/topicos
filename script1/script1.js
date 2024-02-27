e6764d1af9f926fde7329672b9904e95be309a24
grum <al22760591@ite.edu.mx>
2024-02-26 23:37:36 -0800

    Added script1

---
+++ script1/script1.js
@@ -0,0 +1,35 @@
+/*
+    Elabora un script que genere 10 contenedores de
+    tamaño 100 y les agregue un color aleatorio.
+
+    Javier Osvaldo Perez Bretado - 22760591
+
+*/
+
+// Se selecciona el body
+const body = document.querySelector('body')
+
+// Arreglo con los colores disponibles (10 colores)
+const availableColors = [
+    'red', 'blue', 'green', 'yellow', 'black', 
+    'white', 'orange', 'purple', 'pink', 'brown']
+
+
+// Ciclo para crear los 10 contenedores con colores aleatorios
+for (let i = 0; i < 10; i++){
+
+    // Se crea el div
+    const div = document.createElement('div')
+    
+    // Se le asignan las propiedades
+    div.style.width = '100px'
+    div.style.height = '100px'
+    div.style.margin = '10px'
+    
+    // Se le asigna un color aleatorio (Se usa una funcion anonima) 
+    div.style.backgroundColor = availableColors[ Math.floor(Math.random() * 10) ]
+
+    // Se agrega el div al body
+    body.appendChild(div)
+
+}
